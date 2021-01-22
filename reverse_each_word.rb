0cbb6d498d00ff7cf1abba6299c41364550d52cb
Kiana Long <kianajade42.kl@gmail.com>
2021-01-22 18:38:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
-order=["Hello there, and how are you?"]
- def reverse_each_word(order)
-  order.collect do |order|
-  order
-  end
+def reverse_each_word(order)
+order.reverse.split.reverse.join(" ")
 end
